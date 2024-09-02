32caa54ab0a9fdb005fdd7ddc78da9a7d57e6c8f
Amit Mishra <amitmishra74880@gmail.com>
2024-09-02 08:29:26 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,22 +17,22 @@ app.use(cors(corsOptions));
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(requestLimiter)
-
+app.use(requestLimiter);
 
 // APIs
 app.use('/', AllRoutes);
 app.use("/", (req, res) => res.send("Welcome to foodie_hub backend Zone!"));
 
-
-app.listen(8080, () => {
-    console.log(`Server is running on port ${8080}`);
-});
 // Synchronize the models with the database and start the server
 sequelize.sync({ alter: false })
     .then(() => {
         console.log('Database & tables synced successfully');
+        app.listen(8080, () => {
+            console.log(`Server is running on port ${8080}`);
+        });
     })
     .catch(err => {
         console.error('Database sync error:', err);
+        // Optionally, you could terminate the process here if sync fails
+        process.exit(1);
     });
